f2063145e9c41972a2ab4e70568c8aa00ae3fbe5
jan krupinski <jk.krupinski@gmail.com>
2021-01-22 20:45:21 +0100

    Add templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import { Page } from './templates/Page';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Page>
+        <Navbar/>
+      </Page>
     </BrowserRouter>
   );
 }
