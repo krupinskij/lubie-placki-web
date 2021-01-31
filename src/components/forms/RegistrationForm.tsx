f921100db8b6ab30e44f62ec92a25405f75744ac
jan krupinski <jk.krupinski@gmail.com>
2021-01-31 18:59:49 +0100

    [LOGIN USER] Add login form to user panel

---
+++ src/components/forms/RegistrationForm.tsx
@@ -0,0 +1,90 @@
+import { Formik } from 'formik';
+import React from 'react';
+import { Mutation, MutationFunction, MutationResult, OperationVariables } from 'react-apollo';
+import * as Yup from 'yup';
+import { TextField, EmailField, PasswordField } from '../../components/form/Field';
+import { FormActions, FormContainer, FormFields } from '../../components/form/Form';
+import { FormLink } from '../../components/form/FormLink';
+import { SubmitButton } from '../../components/button/Button';
+import { REGISTER_MUTATION } from '../../graphql/register.mutation';
+import { UserSession } from '../../utils/user-session';
+import { RouteComponentProps, withRouter } from 'react-router';
+
+const registerValidationSchema = Yup.object().shape({
+    email: Yup.string().required().email(),
+    username: Yup.string().required().min(5),
+    password: Yup.string().required().min(5),
+    repeatPassword: Yup.string().required().oneOf([Yup.ref('password')])
+});
+
+function RegistrationForm({ history }: RouteComponentProps<void>) {
+
+    const register = (
+        trigger: MutationFunction<any, OperationVariables>,
+        registerInput: any
+    ) => {
+        return trigger({ 
+            variables: { 
+                credentials: registerInput 
+            } 
+        })
+        .then(resp => {
+            const token = resp.data.register.token;
+            UserSession.saveToken(token);
+            history.push("/");
+        })
+        .catch(err => console.log(err));
+    }
+    
+    return(
+        <Mutation mutation={REGISTER_MUTATION}>
+            {(trigger: MutationFunction<any, Record<string, any>>, result: MutationResult<any>) => ( 
+                <Formik
+                    validationSchema={registerValidationSchema}
+                    validateOnBlur={true}
+                    validateOnChange={true}
+                    onSubmit={registerInput => {
+                        register(trigger, registerInput);
+                    }}
+                    initialValues={{
+                        username: '',
+                        email:'',
+                        password: '',
+                        repeatPassword: ''
+                    }}
+                >
+                    { ({ isValid }) => <FormContainer title="Zarejestruj się">
+                        <FormFields>
+                            <TextField 
+                                label="Podaj nazwę użytkownika:" 
+                                name="username" 
+                                placeholder="Nazwa użytkownika"
+                                required={ true }
+                            />
+                            <EmailField 
+                                label="Podaj email:"
+                                required={ true }
+                            />
+                            <PasswordField 
+                                label="Podaj hasło:" 
+                                required={ true }
+                            />
+                            <PasswordField 
+                                label="Powtórz hasło:" 
+                                name="repeatPassword" 
+                                placeholder="Powtórz hasło"
+                                required={ true }
+                            />
+                        </FormFields>
+                        <FormActions>
+                            <SubmitButton disabled={ !isValid } text="Zarejestruj się"/>
+                        </FormActions>
+                        <FormLink pretext="Masz już konto?" text="Zaloguj się" to="/login"/> 
+                    </FormContainer> } 
+                </Formik>
+            )} 
+        </Mutation>
+    )
+}
+
+export const RegistrationFormWithRouter = withRouter(RegistrationForm);
--- src/views/login-page/LoginPage.tsx
+++ src/views/login-page/LoginPage.tsx
@@ -1,80 +1,14 @@
-import { Formik } from 'formik';
 import React from 'react';
-import { Mutation, MutationFunction, MutationResult, OperationVariables } from 'react-apollo';
-import * as Yup from 'yup';
 import { View } from '../../templates/View';
 import { Component } from '../../templates/Component';
-import { EmailField, PasswordField } from '../../components/form/Field';
-import { FormActions, FormContainer, FormFields } from '../../components/form/Form';
-import { FormLink } from '../../components/form/FormLink';
-import { SubmitButton } from '../../components/button/Button';
-import { LOGIN_MUTATION } from '../../graphql/login.mutation';
-import { UserSession } from '../../utils/user-session';
-import { RouteComponentProps, withRouter } from 'react-router';
+import { LoginFormWithRouter } from '../../components/forms/LoginForm';
 
-const loginValidationSchema = Yup.object().shape({
-    email: Yup.string().required().email(),
-    password: Yup.string().required().min(5)
-});
-
-function LoginPage({ history }: RouteComponentProps<void>) {
-
-    const login = (
-        trigger: MutationFunction<any, OperationVariables>,
-        loginInput: any
-    ) => {
-        return trigger({ 
-            variables: { 
-                credentials: loginInput 
-            } 
-        })
-        .then(resp => {
-            const token = resp.data.login.token;
-            UserSession.saveToken(token);
-            history.push("/");
-        })
-        .catch(err => console.log(err));
-    }
-    
+export function LoginPage() {
     return(
         <View>
             <Component>
-                <Mutation mutation={LOGIN_MUTATION}>
-                    {(trigger: MutationFunction<any, Record<string, any>>, result: MutationResult<any>) => ( 
-                        <Formik
-                            validationSchema={loginValidationSchema}
-                            validateOnBlur={true}
-                            validateOnChange={true}
-                            onSubmit={loginInput => {
-                                login(trigger, loginInput);
-                            }}
-                            initialValues={{
-                                email:'',
-                                password: ''
-                            }}
-                        >
-                            { ({ isValid }) => <FormContainer title="Zaloguj się">
-                                <FormFields>
-                                    <EmailField 
-                                        label="Podaj email:"
-                                        required={ true }
-                                    />
-                                    <PasswordField 
-                                        label="Podaj hasło:"
-                                        required={ true }
-                                    />
-                                </FormFields>
-                                <FormActions>                                    
-                                    <SubmitButton disabled={ !isValid } text="Zaloguj się"/>
-                                </FormActions>
-                                <FormLink pretext="Nie masz jeszcze konta?" text="Zarejestuj się" to="/register"/> 
-                            </FormContainer> }
-                        </Formik>
-                    )} 
-                </Mutation>
+                <LoginFormWithRouter/>
             </Component>
         </View>
     )
 }
-
-export const LoginPageWithRouter = withRouter(LoginPage);
