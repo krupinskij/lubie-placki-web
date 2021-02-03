f566fc7049117652c33035156c1b6cac91bd4dc6
Jan Krupinski <jk.krupinski@gmail.com>
2021-02-04 00:27:13 +0100

    [CLEANUP] Remove config from lint-staged. Test linter

--- src/views/recipe-page/RecipePage.tsx
+++ src/views/recipe-page/RecipePage.tsx
@@ -9,32 +9,31 @@ import { RECIPE_QUERY } from '../../graphql/recipe.query';
 import { Recipes } from '../../typings/types';
 
 interface RecipeMatchParams {
-    id: string;
+  id: string;
 }
 
 type RecipeProps = RouteComponentProps<RecipeMatchParams>;
 
 export function RecipePage(props: RecipeProps) {
-    
-    return(
-        <View>
-            <Query
-                query={RECIPE_QUERY}
-                variables={{
-                    id: props.match.params.id
-                }}
-            >
-                {({ data, error }: QueryResult<Recipes.Recipe>) => {
-                    if(data) {
-                        return <Recipe {...(data as any).recipe} />;
-                    }
-                    if(error) {
-                        <div>Wystąpił błąd</div>
-                    }
+  return (
+    <View>
+      <Query
+        query={RECIPE_QUERY}
+        variables={{
+          id: props.match.params.id,
+        }}
+      >
+        {({ data, error }: QueryResult<Recipes.Recipe>) => {
+          if (data) {
+            return <Recipe {...(data as any).recipe} />;
+          }
+          if (error) {
+            <div>Wystąpił błąd</div>;
+          }
 
-                    return <div>Loading...</div>
-                }}
-            </Query>
-        </View>
-    )
+          return <div>Loading...</div>;
+        }}
+      </Query>
+    </View>
+  );
 }
