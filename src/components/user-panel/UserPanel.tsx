6bbed75ff3c2e8df2dbdce779fb7ca83a87d7f58
krupinskij <jk.krupinski@gmail.com>
2021-03-20 21:39:34 +0100

    🎨 Material UI: Use material-ui in user panel

--- src/components/user-panel/UserPanel.tsx
+++ src/components/user-panel/UserPanel.tsx
@@ -1,13 +1,40 @@
+import { Avatar, Button, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import { UserSession } from '../../utils/user-session';
 
-import './UserPanel.scss';
-import { UserPanelHeader, UnknownUserPanelBody, UserPanelBody } from './UserPanelContent';
+const useStyles = makeStyles({
+  userPanelStyles: {
+    position: 'fixed',
+    top: 10,
+    right: 10,
+  },
+  loggedUserPanelStyles: {
+    display: 'flex',
+    gap: 5,
+  },
+});
 
 export function UserPanel() {
+  const { userPanelStyles } = useStyles();
+
+  return <div className={userPanelStyles}>{UserSession.isActive ? <LoggedUserPanel /> : <UnloggedUserPanel />}</div>;
+}
+
+function UnloggedUserPanel() {
+  const { loggedUserPanelStyles } = useStyles();
+
   return (
-    <div className="user-panel">
-      <UserPanelHeader />
-      {UserSession.isActive ? <UserPanelBody /> : <UnknownUserPanelBody />}
+    <div className={loggedUserPanelStyles}>
+      <Button variant="outlined" component={Link} to="/login">
+        Zaloguj się
+      </Button>
+      <Button variant="outlined" component={Link} to="/register">
+        Zarejestruj się
+      </Button>
     </div>
   );
+}
+
+function LoggedUserPanel() {
+  return <Avatar aria-label="recipe">A</Avatar>;
 }
