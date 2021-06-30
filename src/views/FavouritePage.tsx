b0fbd84fd11dbe13230715f9179e044487b3e263
krupinskij <jk.krupinski@gmail.com>
2021-06-30 10:16:18 +0200

    Favourite: Add favourite page

---
+++ src/views/FavouritePage.tsx
@@ -0,0 +1,24 @@
+import { Query, QueryResult } from 'react-apollo';
+
+import { Error } from '../components/shared/Error';
+import { Loading } from '../components/shared/Loading';
+import { RecipeList } from '../components/recipe/RecipeList';
+
+import { RECIPES_QUERY } from '../graphql/recipes.query';
+import { Recipes } from '../typings/types';
+
+export function FavouritePage() {
+  return (
+    <Query query={RECIPES_QUERY}>
+      {({ data, loading }: QueryResult<Recipes.Recipes>) => {
+        if (data) {
+          return <RecipeList recipes={data.recipes} />;
+        }
+        if (loading) {
+          return <Loading />;
+        }
+        return <Error />;
+      }}
+    </Query>
+  );
+}
