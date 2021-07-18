31c941ed6e1bc1db8ad937d0a3ef832cd4487b53
krupinskij <jk.krupinski@gmail.com>
2021-07-18 23:38:48 +0200

    Profile Page: Create link from recipe to owner profile

--- src/components/recipe/Recipe.tsx
+++ src/components/recipe/Recipe.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import { useMutation } from 'react-apollo';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -98,7 +98,11 @@ export function Recipe({
           </IconButton>
         }
         avatar={<Avatar aria-label="recipe">A</Avatar>}
-        title={owner.username}
+        title={
+          <Link href={`/profile/${owner._id}`} color="inherit">
+            {owner.username}
+          </Link>
+        }
         subheader={getFullDate(createdAt)}
       />
       <CardMedia
@@ -118,7 +122,7 @@ export function Recipe({
           <ExpandMoreIcon />
         </IconButton>
         <Typography align="center" gutterBottom variant="h3" component="h2">
-          <Link className={styles.link} to={`/recipe/${_id}`}>
+          <Link className={styles.link} underline="none" href={`/recipe/${_id}`}>
             {name}
           </Link>
         </Typography>
