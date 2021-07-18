e354ff986c1d3154648391d6b02a55e7422f67bc
krupinskij <jk.krupinski@gmail.com>
2021-07-18 22:29:07 +0200

    Pagination: Add pagination to recipes lists

--- src/components/recipe/RecipeList.tsx
+++ src/components/recipe/RecipeList.tsx
@@ -1,45 +1,14 @@
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-
+import { PaginatedList } from '../pagination/PaginatedList';
 import { Recipe } from './Recipe';
 
-import { Recipes } from '../../typings/types';
-
-import stand from '../../assets/stand.svg';
+import { DocumentNode } from 'graphql';
 
 interface RecipeListProps {
-  recipes: Recipes.Recipe[];
+  dataName: string;
+  query: DocumentNode;
+  variables?: {};
 }
 
-const useStyles = makeStyles({
-  paper: {
-    padding: '30px 100px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  image: {
-    height: 150,
-    width: 150,
-    margin: 20,
-  },
-});
-
-export function RecipeList({ recipes }: RecipeListProps) {
-  const styles = useStyles();
-
-  if (!recipes.length) {
-    return (
-      <Paper className={styles.paper} elevation={12}>
-        <Typography align="center" variant="h4" component="p">
-          Ludzie! Tu niczego nie ma!
-        </Typography>
-        <img className={styles.image} src={stand} alt="stand icon" />
-      </Paper>
-    );
-  }
-
-  const recipesList = recipes.map((recipe) => <Recipe key={recipe._id} {...recipe} />);
-  return <>{recipesList}</>;
+export function RecipeList({ dataName, query, variables }: RecipeListProps) {
+  return <PaginatedList query={query} dataName={dataName} variables={variables} component={Recipe} />;
 }
--- src/graphql/type-recipes.query.ts
+++ src/graphql/type-recipes.query.ts
@@ -1,28 +1,32 @@
 import gql from 'graphql-tag';
 
 export const TYPE_RECIPES_QUERY = gql`
-  query Recipe($type: String!) {
-    typeRecipes(type: $type) {
-      _id
-      name
-      description
-      ingredients {
-        product
-        quantity
-        unit
-      }
-      directions {
-        text
-      }
-      hints {
-        text
-      }
-      createdAt
-      owner {
+  query TypeRecipes($paginationInput: PaginationInput!, $type: String!) {
+    typeRecipes(pageInput: $paginationInput, type: $type) {
+      data {
         _id
-        username
+        name
+        description
+        ingredients {
+          product
+          quantity
+          unit
+        }
+        directions {
+          text
+        }
+        hints {
+          text
+        }
+        createdAt
+        owner {
+          _id
+          username
+        }
+        isFavourite
+        photo
       }
-      isFavourite
+      pages
     }
   }
 `;
