52cdf3c9a306fd1fdd4b808a2b97f7bbaf70ab7a
krupinskij <jk.krupinski@gmail.com>
2021-07-19 00:00:31 +0200

    Profile Page: Show user recipes

---
+++ src/graphql/user-recipes.query.ts
@@ -0,0 +1,32 @@
+import gql from 'graphql-tag';
+
+export const USER_RECIPES_QUERY = gql`
+  query UserRecipes($paginationInput: PaginationInput!, $owner: String!) {
+    userRecipes(pageInput: $paginationInput, owner: $owner) {
+      data {
+        _id
+        name
+        description
+        ingredients {
+          product
+          quantity
+          unit
+        }
+        directions {
+          text
+        }
+        hints {
+          text
+        }
+        createdAt
+        owner {
+          _id
+          username
+        }
+        isFavourite
+        photo
+      }
+      pages
+    }
+  }
+`;
--- src/views/ProfilePage.tsx
+++ src/views/ProfilePage.tsx
@@ -1,15 +1,23 @@
 import { useQuery } from 'react-apollo';
 import { useParams } from 'react-router';
 import { UserProfile } from '../components/profile/UserProfile';
+import { RecipeList } from '../components/recipe/RecipeList';
 import { Error } from '../components/shared/Error';
 import { Loading } from '../components/shared/Loading';
+import { USER_RECIPES_QUERY } from '../graphql/user-recipes.query';
 import { USER_QUERY } from '../graphql/user.query';
 
 export function ProfilePage() {
   const params: any = useParams();
   const { data, error } = useQuery(USER_QUERY, { variables: { id: params.id } });
 
-  if (data) return <UserProfile {...(data as any).user} />;
+  if (data)
+    return (
+      <>
+        <UserProfile {...(data as any).user} />
+        <RecipeList query={USER_RECIPES_QUERY} variables={{ owner: params.id }} dataName="userRecipes" />;
+      </>
+    );
   if (error) return <Error />;
   return <Loading />;
 }
